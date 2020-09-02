05e37d6bea570e22dc4af25a6f3a64fb07f7d265
Andreas Klamke <andy@klamke.net>
2020-09-03 00:35:42 +0200

    create docker network

--- tests/scripts/setup_phpipam_environment.py
+++ tests/scripts/setup_phpipam_environment.py
@@ -17,17 +17,27 @@ class PhpipamBaseContainer():
         ),
     ]
 
+class PhpipamBaseContainerNetwork():
+    container_network_specs = [
+        dict(
+            name="phpipam_database",
+        ),
+    ]
 
-class PhpipamEnvironment(PhpipamBaseContainer):
+
+class PhpipamEnvironment(PhpipamBaseContainer, PhpipamBaseContainerNetwork):
     def __init__(self, **kwargs):
         self.container_specs = self.container_specs + kwargs.pop('container_specs', [])
+        self.container_network_specs = self.container_network_specs + kwargs.pop('container_network_specs', [])
         self.docker_client = docker.from_env()
 
+    def deploy_phpipam(self):
         for container in self.container_specs:
             self._pull_image(image=container['image'])
 
-    def deploy_phpipam(self):
-        self._create_network()
+        for network in self.container_network_specs:
+            self._create_network(name=network['name'])
+
         for container in self.container_specs:
             self._spawn_container()
 
@@ -36,8 +46,16 @@ class PhpipamEnvironment(PhpipamBaseContainer):
         _image_tag = kwargs.get('tag', 'latest')
         self.docker_client.images.pull(repository=_image_name, tag=_image_tag)
 
+    def _get_network(self, **kwargs):
+        _network_name = kwargs.get('name')
+        return self.docker_client.networks.list(names=_network_name)
+
     def _create_network(self, **kwargs):
-        print("create docker network")
+        _network_name = kwargs.get('name')
+        _network_driver = kwargs.get('driver', 'bridge')
+        _network_exists = self._get_network(name=_network_name)
+        if not _network_exists:
+            self.docker_client.networks.create(name=_network_name, driver=_network_driver)
 
     def _delete_network(self, **kwargs):
         pass
