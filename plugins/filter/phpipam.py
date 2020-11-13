605a85a30dab1c5fc8b08b0ec164d080de03a892
Christian Meißner <cme+github@meissner.sh>
2020-11-13 12:41:18 +0100

    Do some style fixes reported by codacy
    * remove unused directory, methods and imports
    * add missing doc-string
    * fix C0123
    * fix D107, D187, D203, D212, D213
    * fix F401
    * fix missing docstring for package

--- plugins/filter/phpipam.py
+++ plugins/filter/phpipam.py
@@ -1,7 +1,5 @@
 # Copyright (c) 2020 Christian Meißner
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-
-
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -10,8 +8,7 @@ import ipaddress
 
 
 class FilterModule(object):
-    """Define useful filter in collection."""
-
+    """Call to Define useful filter in collection."""
     def filters(self):
         """Export filter for ansible.
 
@@ -19,25 +16,27 @@ class FilterModule(object):
         :rtype: dict
         """
         return {
-            'is_subnet': self.is_subnet,
+            'is_subnet': is_subnet,
         }
 
-    def is_subnet(self, children, parent):
-        """Check if a subnet belongs to another.
 
-        First argument is a subnet  second another. If the first subnet belongs to second
+def is_subnet(children, parent):
+    """
+    Check if a subnet belongs to another.
 
-        :param children: [description]
-        :type children: [type]
-        :param parent: [description]
-        :type parent: [type]
-        :return: [description]
-        :rtype: [type]
-        """
-        c = ipaddress.ip_network(children)
-        p = ipaddress.ip_network(parent)
+    First argument is a subnet  second another. If the first subnet belongs to second
 
-        if not c.subnet_of(p) or c == p:
-            return False
-        else:
-            return c.subnet_of(p)
+    :param children: First subnet in cidr format which should belongs to parent
+    :type children: string
+    :param parent: Second subnet where children should belongs to
+    :type parent: string
+    :return: True if children belongs to parent and false if not or both networks are the same.
+    :rtype: bool
+    """
+    c = ipaddress.ip_network(children)
+    p = ipaddress.ip_network(parent)
+
+    if not c.subnet_of(p) or c == p:
+        return False
+    else:
+        return c.subnet_of(p)
