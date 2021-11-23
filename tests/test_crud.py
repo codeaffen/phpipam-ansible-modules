4edd08514821d9422dd8866791ff52aa80e585c6
Christian Meißner <cme+github@meissner.sh>
2021-11-23 22:30:19 +0100

    Prepare crud test wrapper
    Get `crud_test.py` and `conftest.py` from
    theforeman/foreman-ansible-modules repo.
    Try to adapt both for this repo.
    Commented apipie specific stuff.
    Wrapper works now for `live` and `record` but without saving fixtures.
    It needs to be investigated what I miss here and make `record` mode work
    properly.

---
+++ tests/test_crud.py
@@ -0,0 +1,49 @@
+import distutils.version
+import os
+import sys
+from ansible_runner.utils import isinventory
+
+import pytest
+
+from .conftest import TEST_PLAYBOOKS, run_playbook, run_playbook_vcr, get_ansible_version
+
+IGNORED_WARNINGS = [
+    "Activation Key 'Test Activation Key Copy' already exists.",
+]
+
+if sys.version_info[0] == 2:
+    for envvar in os.environ.keys():
+        try:
+            os.environ[envvar] = os.environ[envvar].decode('utf-8').encode('ascii', 'ignore')
+        except UnicodeError:
+            os.environ.pop(envvar)
+
+
+@pytest.mark.parametrize('module', TEST_PLAYBOOKS)
+def test_crud(tmpdir, module, vcrmode):
+    if vcrmode == "live":
+        run = run_playbook(module)
+    else:
+        record = vcrmode == "record"
+        run = run_playbook_vcr(tmpdir, module, record=record)
+    assert run.rc == 0
+
+    _assert_no_warnings(run)
+
+
+@pytest.mark.parametrize('module', TEST_PLAYBOOKS)
+def test_check_mode(tmpdir, module):
+    run = run_playbook_vcr(tmpdir, module, check_mode=True)
+    assert run.rc == 0
+
+    _assert_no_warnings(run)
+
+
+def _assert_no_warnings(run):
+    for event in run.events:
+        # check for play level warnings
+        assert not event.get('event_data', {}).get('warning', False)
+
+        # check for task level warnings
+        event_warnings = [warning for warning in event.get('event_data', {}).get('res', {}).get('warnings', []) if warning not in IGNORED_WARNINGS]
+        assert [] == event_warnings, str(event_warnings)
