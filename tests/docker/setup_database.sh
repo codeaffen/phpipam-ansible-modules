22bfcaf82eac1aa737b4e6cddb91b6c57450a31e
Christian Meißner <cme+github@meissner.sh>
2022-06-14 20:07:27 +0200

    switch to podman if available

--- tests/docker/setup_database.sh
+++ tests/docker/setup_database.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if grep -q podman <<< $(docker --version 2> /dev/null) ; then
+  echo "Podman is installed"
+  DOCKER_CMD=$(which podman)
+fi
+
 while ! nc -z "${DB_HOST:-127.0.0.1}" "${DB_PORT:-3306}"; do
   echo "Waiting for database connection..."
   sleep 1
@@ -8,10 +13,13 @@ done
 echo "Database is up"
 
 echo "Creating database ${DB_NAME:-phpipam}"
-docker exec -ti docker_phpipam_1 sh -c 'mysql -h database -u phpipam -pphpipamadmin phpipam < /phpipam/db/SCHEMA.sql'
+${DOCKER_CMD} exec -ti docker_phpipam_1 sh -c 'mysql -h database -u phpipam -pphpipamadmin phpipam < /phpipam/db/SCHEMA.sql'
 
 echo "Activating API"
 mysql -u phpipam -pphpipamadmin -h "${DB_HOST:-127.0.0.1}" phpipam --execute="UPDATE settings SET api=1 WHERE id=1;"
 
 echo "Inserting API application"
 mysql -u phpipam -pphpipamadmin -h "${DB_HOST:-127.0.0.1}" phpipam --execute="INSERT INTO api (app_id, app_code, app_permissions, app_security, app_lock_wait) VALUES ('ansible','aAbBcCdDeEfF00112233445566778899',2,'ssl_token',0);"
+
+echo "Disable forced password reset"
+mysql -u phpipam -pphpipamadmin -h "${DB_HOST:-127.0.0.1}" phpipam --execute="UPDATE users SET passChange = 'No' WHERE username = 'Admin';"
